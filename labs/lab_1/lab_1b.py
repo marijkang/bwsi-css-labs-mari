290b403bdacc8940a0bd409a80d797c1c397602f
marijkang <marijkang@gmail.com>
2026-02-09 00:29:24 -0500

    Trial stash changes

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -6,7 +6,6 @@ then performs the operation and returns the result.
 
 The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
-
 """
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
