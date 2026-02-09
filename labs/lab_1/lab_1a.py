82c3d9d34759710ddff888a46131e32b5e3b0513
marijkang <marijkang@gmail.com>
2026-02-08 22:32:15 -0500

    Add personal intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Mari" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi! My name is Mari, and I'm a sophomore from Massachusetts. I really love learning about science (especially biology) in my free time, and I started learning computer science about two years ago. I'm enjoying it so far and wanted to find some ways to combine these two interests, which led me to BWSI! I also like to play and listen to music, and I've played the violin for 10 years. My favorite ice cream flavor is coffee!")
 
 if __name__ == "__main__":
     main()
